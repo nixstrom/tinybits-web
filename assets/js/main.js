1e8f429805aeb9a513c13396d6ee1551b7bc2c94
Nikki Strømsnes <nikkistromsnes@gmail.com>
2015-01-10 22:13:32 +0100

    Tidy up a bit in main.js (update to Sassentials)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,63 +1,67 @@
 var App = App || {};
 
 App.Namespace = (function() {
-	"use strict";
+  "use strict";
 
-	var X = '',
-			Y = '',
-			Z = '',
+  var X = '',
+      Y = '',
+      Z = '',
 
-			IS_TOUCH = !! ("ontouchstart" in window),
+      IS_TOUCH = !! ("ontouchstart" in window),
 
-			// Store cached references to DOM elements
-			// that will be used over and over again
-			dom = {};
+      // Store cached references to DOM elements
+      // that will be used over and over again
+      dom = {};
 
-	/**
-	 * Initialize function
-	 * @return {void}
-	 */
-	function initialize() {
-		// Set up DOM and cache references
-		_setupDOM();
+  /**
+   * Initialize function
+   * @return {void}
+   */
+  function initialize() {
+    // Set up DOM and cache references
+    _setupDOM();
 
-		// Add event listeners
-		_addEventListeners();
-	}
+    // Add event listeners
+    _addEventListeners();
+  }
 
-	/**
-	 * Set up DOM (create?) elements and cache references for future use
-	 * @return {void}
-	 */
-	function _setupDOM() {
-		dom.$body = $( document.body );
+  /**
+   * Set up DOM (create?) elements and cache references for future use
+   * @return {void}
+   */
+  function _setupDOM() {
+    dom.$body = document.querySelector('body');
+  }
 
-		// Container element for this
-		dom.$wrapper = $();
-	}
+  /**
+   * Attach event listeners to DOM elements
+   */
+  function _addEventListeners() {
+    dom.$body.addEventListener( 'click', _onBodyClicked );
+  }
 
-	/**
-	 * Attach event listeners to DOM elements
-	 */
-	function _addEventListeners() {
-		dom.$body.on( 'click', _onBodyClicked );
-	}
+  /**
+   * Event handler for "click" inside body
+   * @param  {object} event event object
+   * @return {void}
+   */
+  function _onBodyClicked( event ) {
+    alert( 'body is clicked, yo bro!' );
+  }
 
-	/**
-	 * Event handler for "click" inside body
-	 * @param  {object} event event object
-	 * @return {void}
-	 */
-	function _onBodyClicked( event ) {
-		alert( 'body is clicked, yo bro!' );
-	}
+  ////////////////
+  // Public API //
+  ////////////////
 
-	////////////////
-	// Public API //
-	////////////////
-
-	return {
-		initialize: initialize
-	};
+  return {
+    initialize: initialize
+  };
 
 })();
+
+document.onreadystatechange = function () {
+  // Initialize app when document is "ready"
+  if (document.readyState == "complete") {
+    App.Main.initialize();
+  }
+}
